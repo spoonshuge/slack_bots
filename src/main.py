40b5a5d7e6ac466f8dcd6c910f045b91987d90b9
212563094 <joshua.thornton@ge.com>
2016-09-11 21:31:10 -0400

    Modifying settings.

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 from slackclient import SlackClient
+# Local modules
+from src import settings
 
 __purpose__ = """Main file from which the application will run."""
 
-
+slack_client = SlackClient(settings.client_id)
 
--- src/settings.py
+++ src/settings.py
@@ -7,15 +7,15 @@ __location__ = os.path.realpath(
     os.path.join(os.getcwd(), os.path.dirname(__file__)))
 
 # Setup ConfigParser to read in data from config file
-config = ConfigParser()
-config.read(os.path.join(__location__, 'config.conf'))
-auth = ConfigParser()
-auth.read(os.path.join(__location__, 'auth.conf'))
+_config = ConfigParser()
+_config.read(os.path.join(__location__, 'config.conf'))
+_auth = ConfigParser()
+_auth.read(os.path.join(__location__, 'auth.conf'))
 
 # Read in config info
 
 
 # Read in auth info
-client_id = auth.get('CREDENTIALS', 'client_id')
-client_secret = auth.get('CREDENTIALS', 'client_secret')
+client_id = _auth.get('CREDENTIALS', 'client_id')
+client_secret = _auth.get('CREDENTIALS', 'client_secret')
 
